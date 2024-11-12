1ec476b483942b454af3f0798d0a22d1d431157d
Colten <coltenickerson@gmail.com>
2024-11-11 20:10:35 -0500

    feat: developed template of user authentication in registration

---
+++ app/src/main/java/com/example/b07group57/SignUpActivity.java
@@ -0,0 +1,88 @@
+package com.example.b07group57;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailTextView, passwordTextView;
+    private Button Btn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(TeMP); //Replace TeMP with the registration activity screen
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //Initialize our email, password, the button ids being pressed,
+        emailTextView = findViewById(R.id.email); //Email being the text name
+        passwordTextView = findViewById(R.id.password); // Password being the text name
+        Btn = findViewById(R.id.buttonRegister); // Register button
+
+        Btn.onClickListener(new View.onClickListener() { //Listening to button press
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+
+    }
+
+    private void registerNewUser(){
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+
+        //Validations for input email and password
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),
+                    "Please enter a email!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(getApplicationContext(),
+                    "Please enter a password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //Registration is Successful
+                    Toast.makeText(getApplicationContext(),
+                            "Registration successful", Toast.LENGTH_LONG).show();
+
+                    //Intent intent = new Intent(SignUpActivity.this, MainActivity.this);
+                    //startActivity(intent);
+                }
+                else {
+                    //Registration Failed
+                    Toast.makeText(getApplicationContext(),
+                            "Registration failed" + "Please try again later",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
