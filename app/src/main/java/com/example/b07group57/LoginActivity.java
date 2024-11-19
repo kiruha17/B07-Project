0fb3ee6ead532bb3bc22e75aa7669fcaa7d884f4
kiruha17 <wangterry36b@gmail.com>
2024-11-18 22:01:04 -0500

    Initial commit for feature/Terry/integration

--- app/src/main/java/com/example/b07group57/LoginActivity.java
+++ app/src/main/java/com/example/b07group57/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         if (isLoggedIn) {
             if (currentTime - loginTimestamp < thirtyDaysInMillis) {
                 // if login session is still valid, move to homepage
-                startActivity(new Intent(this, HomeActivity.class));
+                startActivity(new Intent(this, LoginHomeFragment.class));
                 finish();
             } else {
                 // if the session has expired, make user login again
@@ -61,7 +61,7 @@ public class LoginActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     // success
                     saveLoginState();
-                    startActivity(new Intent(this, HomeActivity.class)); // HomeActivity.class
+                    startActivity(new Intent(this, LoginHomeFragment.class)); // HomeActivity.class
                     finish();
                 }
                 else {
