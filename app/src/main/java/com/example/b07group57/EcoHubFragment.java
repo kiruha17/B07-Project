143e530a44438e9700b9739407010f40354b0aec
kiruha17 <wangterry36b@gmail.com>
2024-11-24 22:40:46 -0500

    feature: create Eco Hub without implementing any data yet

---
+++ app/src/main/java/com/example/b07group57/EcoHubFragment.java
@@ -0,0 +1,19 @@
+package com.example.b07group57;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EcoHubFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.eco_hub_fragment, container, false);
+    }
+}
