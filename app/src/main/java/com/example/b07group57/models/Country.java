b5b344a783d6ba1dcff45d9431d79807e2889c56
YMichaelHao <michael4hao@gmail.com>
2024-11-28 17:49:42 -0500

    added country files and model/util

---
+++ app/src/main/java/com/example/b07group57/models/Country.java
@@ -0,0 +1,19 @@
+package com.example.b07group57.models;
+
+public class Country {
+    private String name;
+    private String value;
+
+    public Country(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
