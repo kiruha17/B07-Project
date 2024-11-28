0085408c547eef571a2b8aa5694ef21215a4c393
kiruha17 <wangterry36b@gmail.com>
2024-11-28 15:03:16 -0500

    feature: start work on HousingCalculator

---
+++ app/src/main/java/com/example/b07group57/utils/HousingCalculator.java
@@ -0,0 +1,82 @@
+package com.example.b07group57.utils;
+
+import android.util.Log;
+
+import com.example.b07group57.models.SurveyData;
+
+public class HousingCalculator {
+
+    public static double calculateHousing(SurveyData data) {
+
+        double total = 0;
+
+        if (!data.waterHeater.equals(data.homeHeater)) {
+            total += 233;
+        }
+
+        switch (data.renewableEnergy) {
+            case "Yes, primarily (more than 50% of energy use)":
+                total -= 6000;
+                break;
+            case "Yes, partially (less than 50% of energy use)":
+                total -= 4000;
+                break;
+            case "No":
+                break;
+            default:
+                Log.e("calculateHousing",
+                        "None of the renewableEnergy data matches");
+                return Double.NEGATIVE_INFINITY;
+        }
+
+        switch (data.homeType) {
+            case "Detached house":
+                total += calculateDetachedHouse(data.homePeople, data.homeSize, data.homeHeater,
+                        data.electricityBill);
+                break;
+            case "Semi-detached house":
+                total += calculateSemiDetachedHouse(data.homePeople, data.homeSize, data.homeHeater,
+                        data.electricityBill);
+                break;
+            case "Townhouse":
+            case "Other":
+                total += calculateTownHouse(data.homePeople, data.homeSize, data.homeHeater,
+                        data.electricityBill);
+                break;
+            case "Condo/Apartment":
+                total += calculateCondo(data.homePeople, data.homeSize, data.homeHeater,
+                        data.electricityBill);
+                break;
+            default:
+                Log.e("calculateHousing",
+                        "None of the homeType data matches");
+                return Double.NEGATIVE_INFINITY;
+        }
+
+        return total;
+    }
+
+    private static double calculateDetachedHouse(String homePeople, String homeSize,
+                                                  String heater, String electricityBill) {
+
+        return -1;
+    }
+
+    private static double calculateSemiDetachedHouse(String homePeople, String homeSize,
+                                                      String heater, String electricityBill,) {
+
+        return -1;
+    }
+
+    private static double calculateTownHouse(String homePeople, String homeSize,
+                                              String heater, String electricityBill) {
+
+        return -1;
+    }
+
+    private static double calculateCondo(String homePeople, String homeSize,
+                                              String heater, String electricityBill) {
+
+        return -1;
+    }
+}
