545af956a1c853940f3de0f2fbcae4253f42b0c6
Colten Nickerson <coltenickerson@gmail.com>
2024-12-03 02:47:03 -0500

    fix: App Crash Resolved

--- app/src/main/java/com/example/b07group57/EcoTrackerFragment.java
+++ app/src/main/java/com/example/b07group57/EcoTrackerFragment.java
@@ -170,25 +170,6 @@ public class EcoTrackerFragment extends Fragment {
         });
         selectedFuelType = fuelTypeSpinner.getSelectedItem().toString();
 
-
-        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.tracker) {
-                loadFragment(new EcoTrackerFragment());
-            } else if (item.getItemId() == R.id.gauge_nav) {
-                loadFragment(new EcoGaugeFragment());
-            } else if (item.getItemId() == R.id.hub_nav) {
-                loadFragment(new EcoHubFragment());
-            } else if (item.getItemId() == R.id.balance_nav) {
-                loadFragment(new EcoBalanceFragment());
-            } else if (item.getItemId() == R.id.agent_nav) {
-                Toast.makeText(getContext(), "Coming soon!", Toast.LENGTH_LONG).show();
-            }
-            return true;
-
-        });
-
-
         return view;
     }
 
